3a377d3397c051d5b3dfe3545b17fbd8eb6ea250
Christian Schmidt <christiancls93@gmail.com>
2017-08-19 18:07:21 +0200

    First code, not tested

--- Titabel/src/Titabel.java
+++ Titabel/src/Titabel.java
@@ -1,5 +1,24 @@
+
 /**
  * Created by Christian on 19-Aug-17.
  */
 public class Titabel {
+
+
+
+
+    public void tabel (int counter){
+
+        System.out.println("The " + counter + " line is:");
+
+        int runthroug_counter = 1;
+        while (runthroug_counter <= 10){
+            System.out.println("****: " + runthroug_counter*counter);
+            runthroug_counter ++;
+        }
+
+
+    }
+
+
 }
